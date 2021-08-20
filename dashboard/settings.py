13dfec8416002c4a0c4f33c28eb002c28221f14e
kateapl <cakanbr@gmail.com>
2021-08-20 17:27:22 +0300

    add redis

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -76,7 +76,7 @@ CELERY_TASK_TRACK_STARTED = True
 CELERY_TASK_TIME_LIMIT = 30 * 60
 
 CELERY_BROKER_URL = 'amqp://rabbit' #specifies the connection string to the Broker. amqp indicates RabbitMQ is being used as broker.
-#CELERY_RESULT_BACKEND = 'redis://localhost:6379' #sets redis as the result backend. 6379 is the default port.
+CELERY_RESULT_BACKEND = 'redis://redis:6379' #sets redis as the result backend. 6379 is the default port.
 # CELERY_ACCEPT_CONTENT = ['application/json']
 # CELERY_RESULT_SERIALIZER = 'json'
 # CELERY_TASK_SERIALIZER = 'json'
